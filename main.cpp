999dbdbe836ad5a954a2d86fe5a9b804cc0f1a83
AwnAbuhdeib <awn_abuhdeib@hotmail.com>
2022-02-28 14:30:36 +0000

    shortcuts

--- main.cpp
+++ main.cpp
@@ -4,5 +4,9 @@ int main()
 {
         std::printf("%s\n", "Hello World");
 
-        std::printf("test");
+        std::printf("test\n");
+//option shift f to fix the format 
+//command shift p for git commit and git push
 }
+
+
