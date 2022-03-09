145024c7ff290c79d2399404c8542d354d68eced
AwnAbuhdeib <awn_abuhdeib@hotmail.com>
2022-03-09 12:53:22 +0000

    string view

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <cstdio>
+#include <string_view>
+#include <string>
+#include <iostream>
 
 int main()
 {
@@ -7,6 +10,15 @@ int main()
         std::printf("test\n");
 //option shift f to fix the format 
 //command shift p for git commit and git push
+std::string original = "message";
+std::string copy1 = original;
+std::string_view view1 {original};
+original="adjusted message";
+view1 = original;
+ std::cout<<"orgi: "<<original<<std::endl;
+ std::cout<<"copy1: "<<copy1<<std::endl;
+ std::cout<<"view1: "<<view1<<std::endl;
+
 }
 
 
