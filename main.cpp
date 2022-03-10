a96c5711a030e2afc4b50dc4796d33af68178df0
AwnAbuhdeib <awn_abuhdeib@hotmail.com>
2022-03-10 17:33:05 +0000

    strings

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-        std::printf("%s\n", "Hello World");
+     /*   std::printf("%s\n", "Hello World");
 
         std::printf("test\n");
 //option shift f to fix the format 
@@ -17,7 +17,23 @@ original="adjusted message";
 view1 = original;
  std::cout<<"orgi: "<<original<<std::endl;
  std::cout<<"copy1: "<<copy1<<std::endl;
- std::cout<<"view1: "<<view1<<std::endl;
+ std::cout<<"view1: "<<view1<<std::endl;*/
+ 
+ std::string basket = "apples, banana,peach,banana";
+ // size_t is like int
+
+ std::size_t found = basket.find("banana");
+ int length_add = add.length();
+ std::cout <<"the value of found is: "<< found << std::endl;
+ std::string second_fruit = basket.substr(found,7);
+ std::cout<<"the substring value is: "<< second_fruit<<std::endl;
+
+
+ std::string languages = "arabic,french,english,turkish";
+ std::string language_of_intrest = "arabic";
+ std::size_t position = languages.find(language_of_intrest);
+ std::size_t lenght_of_languages = language_of_intrest.lenght();
+ 
 
 }
 
